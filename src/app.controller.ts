03948eea1a546d484eb25df761c3d3e9f7acb243
React | Next | Node <59826852+mahabur-rahman@users.noreply.github.com>
2023-08-21 00:00:24 +0600

    initial commit

---
+++ src/app.controller.ts
@@ -0,0 +1,43 @@
+import { Controller, Get, Post, Put, Delete, Param } from '@nestjs/common';
+import { ReportType, data } from './data';
+// import { AppService } from './app.service';
+
+@Controller('income/:type')
+export class AppController {
+  // constructor(private readonly appService: AppService) {}
+
+  // get all report
+  @Get()
+  getAllReports(@Param('type') type: string) {
+    // console.log(type);
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+
+    return data.report.filter((report) => report.type === reportType);
+  }
+
+  // get single report
+  @Get(':id')
+  getSingleReport(@Param('type') type: string, @Param('id') id: string) {
+    console.log({ type, id });
+    return {};
+  }
+
+  // create report
+  @Post()
+  createReport() {
+    return 'created';
+  }
+
+  // update report
+  @Put(':id')
+  updateReport() {
+    return 'update report';
+  }
+
+  // delete report
+  @Delete(':id')
+  deleteReport() {
+    return 'deleted ';
+  }
+}
---
+++ src/data.ts
@@ -0,0 +1,45 @@
+export enum ReportType {
+  INCOME = 'income',
+  EXPENSE = 'expense',
+}
+
+interface Data {
+  report: {
+    id: string;
+    source: string;
+    amount: number;
+    created_at: Date;
+    updated_at: Date;
+    type: ReportType;
+  }[];
+}
+
+export const data: Data = {
+  report: [
+    {
+      id: 'uuid1',
+      source: 'Salary',
+      amount: 7500,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type: ReportType.INCOME,
+    },
+
+    {
+      id: 'uuid2',
+      source: 'Youtube',
+      amount: 7300,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type: ReportType.INCOME,
+    },
+    {
+      id: 'uuid3',
+      source: 'Food',
+      amount: 300,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type: ReportType.EXPENSE,
+    },
+  ],
+};
